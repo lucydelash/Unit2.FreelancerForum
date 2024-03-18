abc375ecfe94df4325715aa02f7bac8e064420a8
lucydelash <lucyrudd614@gmail.com>
2024-03-17 20:10:15 -0400

    added freelancers & average price code to js

---
+++ index.js
@@ -0,0 +1,27 @@
+// create an object of the available freelancers
+  // Alice - writer - $30
+  // Bob - teacher - $50
+const freelancers = [
+
+{ name: `Alice`,
+  Position: `writer`,
+  price: 30
+},
+{ name: `Bob`,
+  Position: `teacher`,
+  price: 50
+}]
+
+// write a function to average out the price between the listed freelancers
+func updateList() {
+  const listElement = document.getElementById(`freelancersList`);
+  const averagePriceElement = document.getElementById(`average-price`);
+}
+
+// add carol the developer to the list of freelancers (after Alice & Bob)
+func carolFreelancer = {
+  name: `Carol`,
+  position: `developer`,
+  price: 70
+};
+
